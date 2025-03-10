0db72b5cda5331d8894679eb822a39bf58873c40
Eduardo Santana <ezambomsantana@gmail.com>
2025-03-10 11:07:37 -0300

    add tests

--- src/test/java/br/insper/iam/usuario/service/UsuarioServiceTests.java
+++ src/test/java/br/insper/iam/usuario/service/UsuarioServiceTests.java
@@ -10,6 +10,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -26,11 +28,43 @@ public class UsuarioServiceTests {
     @Test
     void test_findAllUsuariosWhenUsuariosIsEmpty() {
 
+        // preparacao
         Mockito.when(usuarioRepository.findAll()).thenReturn(new ArrayList<>());
 
+        // chamada
         List<Usuario> usuarios = usuarioService.getUsuarios();
 
+        //verificacaoes
         Assertions.assertEquals(0, usuarios.size());
+    }
+
+    @Test
+    void test_saveUsuarioSuccessfully() {
+
+        Usuario usuario = new Usuario();
+        usuario.setEmail("a@a.com");
+        usuario.setNome("Teste");
+
+        Mockito.when(usuarioRepository.save(usuario)).thenReturn(usuario);
+
+        Usuario usuarioReturn = usuarioService.saveUsuario(usuario);
+
+        Assertions.assertEquals("a@a.com", usuarioReturn.getEmail());
+        Assertions.assertEquals("Teste", usuarioReturn.getNome());
+
+    }
+
+    @Test
+    void test_saveUsuarioErrorEmailIsNull() {
+
+        Usuario usuario = new Usuario();
+        usuario.setNome("Teste");
+
+        ResponseStatusException exception = Assertions.assertThrows(
+                ResponseStatusException.class,
+                () -> usuarioService.saveUsuario(usuario));
+        Assertions.assertEquals(HttpStatus.BAD_REQUEST, exception.getStatusCode());
+
     }
 
 
