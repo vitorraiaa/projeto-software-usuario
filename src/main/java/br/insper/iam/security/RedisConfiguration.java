d6fa127faec057dea564abb313239bd0caaf41fe
Eduardo Santana <ezambomsantana@gmail.com>
2025-04-08 22:29:42 -0300

    fix redis

--- src/main/java/br/insper/iam/security/RedisConfiguration.java
+++ src/main/java/br/insper/iam/security/RedisConfiguration.java
@@ -1,15 +1,31 @@
 package br.insper.iam.security;
 
 import br.insper.iam.usuario.Usuario;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfiguration {
+
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisHost, redisPort);
+        return new LettuceConnectionFactory(config); // Ou new JedisConnectionFactory(config);
+    }
 
     @Bean
     public RedisTemplate<String, Usuario> redisTemplate(RedisConnectionFactory factory) {
